d875c49a3b3267e54b319d8eea528cfe5eea923b
iwatts3519 <iwatts@me.com>
2021-04-30 11:09:41 +0100

    Initial Commit

---
+++ datavizFunctions.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from os import path
+from PIL import Image
+from wordcloud import WordCloud, STOPWORDS
+from sklearn.feature_extraction.text import TfidfVectorizer
+import preprocessing
+
+df = preprocessing.df
+
+
+def createWordCloud(company_number):
+    font_path = './Sigmar_One/SigmarOne-Regular.ttf'
+    mask_path = './imageonline-co-textimage2.png'
+    mask = np.array(Image.open(mask_path))
+    text = df['Job Description'][company_number]
+    company = df["Company Name"][company_number]
+    wordcloud = WordCloud(width=1000, height=500, background_color='White', contour_width=3, contour_color='black',
+                          stopwords=STOPWORDS, font_path=font_path, mask=mask).generate(text)
+    return company, wordcloud
+
+
+def create_TopNFeatures(company, n_features):
+    text = df['Job Description'][int(company):int(company) + 1]
+    vectoriser = TfidfVectorizer(stop_words='english')
+    model = vectoriser.fit_transform(text)
+    indices = np.argsort(vectoriser.idf_)[::-1]
+    keywords = vectoriser.get_feature_names()
+    top_n_features = [keywords[i] for i in indices[:n_features]]
+    return top_n_features
--- index.py
+++ index.py
@@ -1,5 +1,14 @@
 import preprocessing
+import datavizFunctions as fx
+import matplotlib.pyplot as plt
 
 job_listings = preprocessing.df
 
 print(job_listings)
+
+comp, cloud = fx.createWordCloud(3)
+
+print(comp)
+plt.imshow(cloud)
+plt.axis('off')
+plt.show()
